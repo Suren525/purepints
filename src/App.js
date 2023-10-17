2d5c0cc3f8eb61a9c86e7171a81d45b495ac2d56
Suri K <suren8990@gmail.com>
2023-10-17 18:25:48 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import Home from './pages/Home';
+import AboutUs from './pages/AboutUs';
+import ContactUs from './pages/ContactUs';
+import { Route, Routes } from 'react-router-dom';
+import beer from './images/beer.mp4';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Navbar />
+      <div className='container'>
+      <video src={beer} autoPlay loop muted />
+        <Routes>
+         <Route path="/home" element={<Home />} />
+          <Route path='/aboutUs' element={<AboutUs />} />
+          <Route path='/contactUs' element={<ContactUs />} />
+        </Routes>
+      </div>
+    </>
+  )
 }
 
 export default App;
